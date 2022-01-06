5c28e42f89e725cee2c25a27a124a0214a2fa017
Audrey Odiaka <anitaudrey@gmail.com>
2022-01-06 10:17:03 +0100

    Add slider to amount

--- app/models/loaner.rb
+++ app/models/loaner.rb
@@ -1,2 +1,5 @@
 class Loaner < ApplicationRecord
+    validates :borrowed_amonunt, presence: true, numericality: { in: 1..200000 }
+    # validates :borrowed_amonunt, numericality: { in: 18..65 }
+    validates :duration, presence: true, numericality: { only_integer: true }
 end
