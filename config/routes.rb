cbfea92d7d0dd20d7e0a28d4d73933156caf0401
Audrey Odiaka <anitaudrey@gmail.com>
2022-01-14 15:07:53 +0100

    Admin user devise add

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  # get 'static_pages/home'
   resources :profiles
   devise_for :users
   resources :loaners, only: [:index, :new, :show, :create]
   resources :steps_after_registration 
-  root 'loaners#index'
+  # root 'loaners#index'
+  root 'static_pages#home'
   get 'declaration', to: "profiles#download_pdf"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
